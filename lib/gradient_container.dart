11e591ec01b84ed3e2a429e997a1b7eeeaccb7aa
ParasSingla-009 <chutku202@gmail.com>
2024-06-15 16:35:57 +0530

    first commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/first_page.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.colors});
+  final flutterimage = 'assets/images/quiz-logo.png';
+  final List<Color> colors;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: colors, begin: startAlignment, end: endAlignment),
+      ),
+      child:  const Center(
+        child: FirstPage()
+      ),
+    );
+  }
+}
